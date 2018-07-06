0d458eaf301c6a894ea0b5de1fd30d4eb70be4cb
vsevolod.alexeev <vsevolod.alexeev@osinit.com>
2018-07-06 16:24:16 +0300

    display arrayList element

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,6 +3,8 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -25,7 +27,15 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
-        Log.i("NumberActivity", "Word at index 0:" + words.get(0));
-        Log.i("NumberActivity", "Word at index 9:" + words.get(9));
+        //find the parent View
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+        //create new View element
+        TextView wordView = new TextView(this);
+        wordView.setText(words.get(0));
+
+        //add new View in the parent View
+        rootView.addView(wordView);
+
     }
 }
