4b821c6c7a7dd5dbb86164ad2f6aa0a6b3a9c486
Vsevolod Alexeev <ovarb6@gmail.com>
2018-07-09 03:08:14 +0300

    ListView adapter is added

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,7 +3,9 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -27,40 +29,22 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
-        //find the parent View
-        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+        // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
+        // adapter knows how to create layouts for each item in the list, using the
+        // simple_list_item_1.xml layout resource defined in the Android framework.
+        // This list item layout contains a single {@link TextView}, which the adapter will set to
+        // display a single word.
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
 
-        //create a variable to keep track of the current index position
-        /*int index = 0;
-        int arrayListLength = words.size();
-        while (index < arrayListLength) {
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // activity_numbers.xml layout file.
+        ListView listView = (ListView) findViewById(R.id.list);
 
-            //create new {@link textView} that displayed the word at index position element
-            TextView wordView = new TextView(this);
-            wordView.setText(words.get(index));
-            //add the View as a child to the rootView
-            rootView.addView(wordView);
-
-            //update counter variable
-            index++;
-        }*/
-
-        int arrayListLength = words.size();
-
-        for (int index = 0; index < arrayListLength; index++) {
-
-            //create new {@link textView} that displayed the word at index position element
-            TextView wordView = new TextView(this);
-            wordView.setText(words.get(index));
-            //add the View as a child to the rootView
-            rootView.addView(wordView);
-        }
-
-
-
-
-
-
-
+        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
+        // {@link ListView} will display list items for each word in the list of words.
+        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
+        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
+        listView.setAdapter(itemsAdapter);
     }
 }
