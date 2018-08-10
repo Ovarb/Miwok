74f760082f36cf02ed7f7325bc88fde4b6158f7e
vsevolod.alexeev <vsevolod.alexeev@osinit.com>
2018-08-10 20:52:58 +0300

    add touch reaction to Family category button

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -18,6 +18,7 @@ package com.example.android.miwok;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
@@ -44,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         TextView buttonFamily = (TextView) findViewById(R.id.family);
+
         buttonFamily.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
